be9f33ab7a7889af970a5c7250d6c5f5ce26958b
Sam Muir <sam.muir59@gmail.com>
2020-03-11 23:20:36 +1300

    Fixed style bugs for mobile responsiveness. Finished Intro component.

--- src/ts/components/dumb/intro/intro.tsx
+++ src/ts/components/dumb/intro/intro.tsx
@@ -13,12 +13,20 @@ export class Intro extends React.Component<any, any> {
     return (
       <div id="intro">
         <div id="introHead" />
+        <div id="introContent">
           <div id="authorInfo">
-            Lorum ipsum
+            by <span id="author">Liam Coxon</span>
+            <br />
+            Contributor
+            <br />
+            <span id="introDate">March 10 2020</span>
           </div>
-          <div id="introContent">
-            Lorum ipsum
+          <div id="introText">
+            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           </div>
+        </div>
         <div id="introFoot" />
       </div>
     );
